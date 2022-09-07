5a98555f2bbe6f6701512c2a3bd67777407a7113
Jean-Michel Friedt <friedtj@free.fr>
2022-09-07 17:53:31 +0200

    update Python script

--- 220830_OP/godual_ranging_OP.py
+++ 220830_OP/godual_ranging_OP.py
@@ -51,25 +51,27 @@ def ranging(filename, prn_code):
             multmp1 = np.fft.fftshift(np.fft.fft(y) * fcode)
             multmp2 = np.fft.fftshift(np.fft.fft(d2) * fcode)
             interpolation1=np.concatenate( (np.zeros(len(y))+1j*np.zeros(len(y)) , multmp1, np.zeros(len(y))+1j*np.zeros(len(y))))
-            multmp1 = np.fft.fftshift(interpolation1)
             interpolation2=np.concatenate( (np.zeros(len(d2))+1j*np.zeros(len(d2)) , multmp2, np.zeros(len(d2))+1j*np.zeros(len(d2))))
-            prnmap01 = np.fft.ifft(interpolation1) # correlation with returned signal
-            prnmap02 = np.fft.ifft(interpolation2) 
+            multmp1 = np.fft.fftshift(interpolation1)
+            multmp2 = np.fft.fftshift(interpolation2)
+            prnmap01 = np.fft.ifft(multmp1)       # correlation with returned signal
+            prnmap02 = np.fft.ifft(multmp2)
 
-            indice1 = abs(prnmap01).argmax()       # only one correlation peak
-            indice2 = abs(prnmap02).argmax() 
-            indice1_lst.append(indice1)
-            indice2_lst.append(indice2)
-            xval1   = prnmap01[indice1]
-            xval2   = prnmap02[indice2]
-            xval1m1 = prnmap01[indice1-1]
+            indice1 = abs(prnmap01).argmax()      # only one correlation peak
+            indice2 = abs(prnmap02).argmax()
+            xval1 = prnmap01[indice1]
+            xval2 = prnmap02[indice2]
+            xval1m1= prnmap01[indice1-1]
             xval1p1 = prnmap01[indice1+1]
             xval2m1 = prnmap02[indice2-1]
             xval2p1 = prnmap02[indice2+1]
 
-            correction1.append((abs(xval1m1)-abs(xval1p1))/(abs(xval1m1)+abs(xval1p1)-2*abs(xval1))/2);
-            correction2.append((abs(xval2m1)-abs(xval2p1))/(abs(xval2m1)+abs(xval2p1)-2*abs(xval2))/2);
-            print(str(p)+": "+str(indice1)+"+"+str(correction1[-1])+" -- "+str(xval1))
+            correction1=((abs(xval1m1)-abs(xval1p1))/(abs(xval1m1)+abs(xval1p1)-2*abs(xval1))/2);
+            correction2=((abs(xval2m1)-abs(xval2p1))/(abs(xval2m1)+abs(xval2p1)-2*abs(xval2))/2);
+            
+            indice1_lst.append(indice1+correction1)
+            indice2_lst.append(indice2+correction2)
+            print(str(p)+": "+str(indice1)+"+"+str(correction1)+" -- "+str(xval1))
             p += 1
 
 ranging("17h05.bin","../amaranth_twstft/220706_TWSTFT/OP_prn22bpskcode0.bin")
