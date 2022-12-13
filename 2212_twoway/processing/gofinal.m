160e90345b3f2849631ceaa01041202d6fa8e122
Jean-Michel Friedt <friedtj@free.fr>
2022-12-13 16:08:33 +0100

    separate the delays of the two channels (ref/measurement)

--- 2212_twoway/processing/gofinal.m
+++ 2212_twoway/processing/gofinal.m
@@ -9,13 +9,12 @@ if (exist('args')==1)
   dirlistmat=filename;
 else
   dirlistmat=dir('./*mat');
-  filename=dirlistmat(1).name
 end
 
 for dirnummat=1:length(dirlistmat)
+  filename=dirlistmat(dirnummat).name
   datename=str2num(filename(end-13:end-4))+10; % 10 seconds before the emission starts
   clear code
-  filename
   eval(['load ',filename])
   solution1=indice1+correction1_a;
   OP=strfind(filename,"OP"); 
@@ -33,21 +32,21 @@ for dirnummat=1:length(dirlistmat)
     SNR2r=SNR2r(k);
     SNR2i=SNR2i(k);
     df2=df2(k);
-    solution=(solution1-solution2);  % time difference between reference and measurement
+    [a1,b1]=polyfit([1:length(solution1)],(solution1)/(2*Nint+1)/fs,2);
+    [a2,b2]=polyfit([1:length(solution2)],(solution2)/(2*Nint+1)/fs,2);
   else
-    solution=(solution1);            % arbitrary reference time
+    [a1,b1]=polyfit([1:length(solution1)],(solution1)/(2*Nint+1)/fs,2);
   end
-  [a,b]=polyfit([1:length(solution)],(solution)/(2*Nint+1)/fs,2);
   codelen(dirnummat)=length(code)/2;
   if (affiche==1)
     figure
     subplot(221)
-    plot((solution)/(2*Nint+1)/fs); % ranging solution
+    plot((solution2-solution1)/(2*Nint+1)/fs); % ranging solution
     xlabel('time (s)')
     ylabel('ranging delay (s)')
     subplot(223)
-    plot((solution)/(2*Nint+1)/fs-b.yf); % ranging solution
-    legend(num2str(std((solution)/(2*Nint+1)/fs-b.yf)))
+    plot((solution2-solution1)/(2*Nint+1)/fs-(b2.yf-b1.yf)); % ranging solution
+    legend(num2str(std((solution2-solution1)/(2*Nint+1)/fs-(b2.yf-b1.yf))))
     xlabel('time (s)')
     ylabel('delay - parabolic fit (s)')
     subplot(222);plot(10*log10(SNR1r+SNR1i)(k))
@@ -55,15 +54,15 @@ for dirnummat=1:length(dirlistmat)
       subplot(224);plot(10*log10(SNR2r+SNR2i)(k))
     end
   end
-  standard(dirnummat)=std((solution)/(2*Nint+1)/fs-b.yf);
-  average(dirnummat)=mean((solution)/(2*Nint+1)/fs-b.yf);
+  standard(dirnummat)=std((solution1)/(2*Nint+1)/fs-b1.yf);
+  average(dirnummat)=mean((solution1)/(2*Nint+1)/fs-b1.yf);
   printf("%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\r\n");
   for p=1:length(SNR1i)
      % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
      if (OP==0)
-        printf("%s\t%.12f\t%.3f\t%.1f\t%.1f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(datename+p*(length(code)/2)/2.5e6)),solution(p)/(2*Nint+1)/fs,df1(p),10*log10(SNR1i(p)+SNR1r(p)),df2(p),10*log10(SNR2i(p)+SNR2r(p)));
+        printf("%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.1f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(datename+p*(length(code)/2)/2.5e6)),solution1(p)/(2*Nint+1)/fs,df1(p),10*log10(SNR1i(p)+SNR1r(p)),solution2(p)/(2*Nint+1)/fs,df2(p),10*log10(SNR2i(p)+SNR2r(p)));
      else
-        printf("%s\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(datename+p*(length(code)/2)/2.5e6)),solution(p)/(2*Nint+1)/fs,df1(p),10*log10(SNR1i(p)+SNR1r(p)));
+        printf("%s\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(datename+p*(length(code)/2)/2.5e6)),solution1(p)/(2*Nint+1)/fs,df1(p),10*log10(SNR1i(p)+SNR1r(p)));
      end
   end
 end
