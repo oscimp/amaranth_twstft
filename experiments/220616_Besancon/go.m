90f26c5e4965146100e124a6aa313dbb4d43e82b
Jean-Michel Friedt <friedtj@free.fr>
2023-01-12 17:53:06 +0100

    correct MDJ abscissa

--- experiments/220616_Besancon/go.m
+++ experiments/220616_Besancon/go.m
@@ -1,42 +1,62 @@
 fs=5e6
 
+% https://kotetsu1701.com/blog/programing-language-octave/
+% Julian Day
+function julian = julianDay(year, month, day)
+    branch = year + (month - 1.0) / 12.0 + day / 365.25;
+
+    if (floor(month) < 3)
+        month = month + 12.0;
+        year = year - 1.0;
+    endif
+
+    if (branch >= 1582.78)
+        julian = floor(year * 365.25) + floor(year / 400.0) - floor(year / 100.0) + floor(30.59 * (month - 2.0)) + day + 1721088.5;
+    elseif (branch >= 0.0)
+        julian = floor(year * 365.25) + floor(30.59 * (month - 2.0)) + day + 1721086.5;
+    elseif (year < 0.0)
+        julian = sign(year) * floor(abs(year) * 365.25) + floor(30.59 * (month - 2.0)) + day + 1721085.5;
+    end
+end
+
+subplot(211)
 x=load('SATREranging');
-temps=x(:,1)*3600*24+x(:,2)*3600+x(:,3)*60+x(:,4);
+temps=julianDay(2022,6,x(:,1)+x(:,2)/24+x(:,3)/24/60+x(:,4)/24/60/60);
 temps1=temps(1);
 %temps=temps-temps(1);
-plot(temps+7200,x(:,5)/1e9,'r.')
+plot(temps(1:60:end)-2400000.5-59000,x(1:60:end,5)/1e9,'r.')
 hold on
 
-initemps=0;
-dirlist=dir('./*mat');
+dirlist=dir('./*mat.gz');
 for dirnum=1:length(dirlist)
   eval(['load ',dirlist(dirnum).name]);
-  filname=strrep(dirlist(dirnum).name,'.mat','');
-  temps=localtime(str2num(filname));
+  filname=strrep(dirlist(dirnum).name,'.mat.gz','');
+  temps=str2num(filname)/86400+40587-59000;
   solution1=indice1+correction1;
   solution2=indice2+correction2;
-  heure=(temps.mday*24+temps.hour)*ones(1,length(solution1));
-  secon=[0:length(solution1)-1]+temps.min*60;
-  temp=heure*3600+secon;
 %  if (initemps==0)
 %     initemps=temp(1);
 %  end
-  temp=temp-initemps;
   hold on
   s=(solution1-solution2)/fs;
   sk=find(s<0);
   if (isempty(sk)==0)
      s(sk)=1+s(sk);
   end
-  plot(temp,s,'o'); % ranging solution
+  plot(temps,mean(s),'o'); % ranging solution
   mondf(dirnum)=mean(df);
+  mont(dirnum)=temps(1);
 %  sol2=(solution1-solution2)/fs;
 %  xlabel('time (s)')
 %  ylabel('ranging delay (s)')
 end
+ylim([.26285 .263])
+ylabel('ranging delay (s)')
+xlabel('MJD-59000 (day)')
+return
 
 figure
-plot(mondf)
+plot((mont-mont(1))/3600,mondf)
 
 if (1==0)
 subplot(212)
