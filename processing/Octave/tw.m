9338040ec9e056aeabe00c7f1c527b6303a9cc70
Jean-Michel Friedt <friedtj@free.fr>
2023-01-13 10:22:13 +0100

    TW analysis script

--- processing/Octave/tw.m
+++ processing/Octave/tw.m
@@ -1,29 +1,75 @@
 dirop='OP/'
 dirltfb='LTFB/'
 
+graphics_toolkit('gnuplot')
+
+affiche=1;
+m=1;
+
 dop=dir([dirop,'*.*.*.txt']);
 for l=1:length(dop)
   eval(['op=load(''',dirop,dop(l).name,''');']);
   dltfb=dir([dirltfb,dop(l).name(1:7),'*ltfb.txt']);
   if (isempty(dltfb)==0)
-    printf("%s <> %s: ",dop(l).name,dltfb(1).name)
+    printf("%s <> %s: ",dop(l).name,dltfb(1).name);
     eval(['ltfb=load(''',dirltfb,dltfb(1).name,''');']);
     tempsop=((((op(1,2)*31+op(1,3))*24)+op(1,4)*60)+op(1,5)*60)+op(1,6);
     tempsltfb=((((ltfb(1,2)*31+ltfb(1,3))*24)+ltfb(1,4)*60)+ltfb(1,5)*60)+ltfb(1,6);
     if (tempsop>tempsltfb)
-       difference=tempsop-tempsltfb
+       difference=tempsop-tempsltfb;
        ltfb=ltfb(difference+1:end,:);
+       if (length(ltfb>length(op))) ltfb=ltfb(1:length(op),:);end
        op=op(1:length(ltfb),:);
     else
-       difference=tempsltfb-tempsop
+       difference=tempsltfb-tempsop;
        op=op(difference+1:end,:);
        ltfb=ltfb(1:length(op),:);
     end
+    printf(" diff=%d\n",difference)
+    k=find(ltfb(:,9)>max(ltfb(:,9)-10));k=k(1)+1;  % SNR criterion
+    ltfb=ltfb(k:end,:); op=op(k:end,:);
+    k=find(op(:,9)>max(op(:,9)-10));k=k(1)+1;  % SNR criterion
+    ltfb=ltfb(k:end,:); op=op(k:end,:);
+    if (mean(ltfb(:,10)-ltfb(:,13))<0) ltfb(:,10)=ltfb(:,10)+1;end
+    if (mean(op(:,10)-op(:,13))<0) op(:,10)=op(:,10)+1;end
     res=0.5*(ltfb(:,10)-ltfb(:,13)-(op(:,10)-op(:,13)));
+    if (affiche==1)
+       if (std(res)<1e-6)
+         figure
+	 % subplot(8,1,m)
+         plot(res*1e9);
+         xlabel('sample number (s)');ylabel('tw difference (ns)');title([dop(l).name,' - ',dltfb(1).name]);
+	 legend(['std=',num2str(std(res)),' <.>=',num2str(mean(res))]);
+       end
+    end
     res=res*1e9;
-    std(res(20:end))
-    mean(res(20:end))
+    if (std(res)<1E3)
+      std(res)
+      mean(res)
+      sta(m)=std(res);
+      moy(m)=mean(res);
+      temps(m)=str2num(dltfb(1).name(1:10))/86400+40587;
+    else
+      if (affiche==1)
+        figure
+        subplot(311)
+        plot(ltfb(:,10)-ltfb(:,13))
+        subplot(312)
+        plot(op(:,10)-op(:,13));
+        subplot(313)
+        plot(res)
+	std(res)
+      end
+    end
+    m=m+1;
   else
-    printf("No match\n");
+    printf("%s: no match\n",dop(l).name);
   end
+end
+
+if (affiche==1)
+  figure
+  k=find(temps>0);
+  subplot(211);plot(temps(k)-59000,moy(k),'x');ylabel('<tw> (ns)')
+  subplot(212);plot(temps(k)-59000,sta(k),'x');ylabel('std(tw) (ns');xlabel('MJD-59000 (days)')
 end
