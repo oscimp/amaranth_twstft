1628be8e912ea2106d1a1aaa3b6fc0848e5a8e54
Jean-Michel Friedt <friedtj@free.fr>
2023-01-16 15:44:26 +0100

    adjust time when processing matlab files + two way analysis

--- processing/Octave/gofinal_ltfb.m
+++ processing/Octave/gofinal_ltfb.m
@@ -1,7 +1,9 @@
 pkg load signal
+format long
 affiche=0;
 fs=5e6;
 Nint=1;
+global code=ones(2*fs,1); % needed if not stored in .mat archive, will be overwritten otherwise
 
 if (affiche==1)
   graphics_toolkit('gnuplot')  % beware of steps with fltk !
@@ -20,86 +22,75 @@ if (exist('args')==1)
   dirlistmatltfb=localltfb;
 else
   dirlistmatltfb=dir([localdirltfb,'/1*mat.gz*']); % exclude remote*
-  dirlistendltfb=dir([localdirltfb,'/1*end*']); % exclude remote*
 end
 
 for dirnummat=1:length(dirlistmatltfb) 
 %%% start LTFB: local
-  filenameltfb=[localdirltfb,dirlistmatltfb(dirnummat).name];
-  dateltfb=str2num(dirlistendltfb(dirnummat).name(1:10));
-  clear code
-  eval(['load ',filenameltfb])
-  solution1lo=indice1+correction1;          % ranging
-  solution2lo=indice2+correction2;          % reference channel = loopback
-  df1lo=df1;
-  df2lo=df2;
-  SNR1lo=10*log10(SNR1r+SNR1i);
-  SNR2lo=10*log10(SNR2r+SNR2i);
-% arbitrarily keep max(SNR)-10 to remove initial record when TX inactive
-  dateini=dateltfb-length(solution1lo)+1; % seconds since 1/1/1970 from loopback
-%%% start LTFB: remote
-  filenameltfb=[remotedirltfb,dirlistmatltfb(dirnummat).name];
-  eval(['load ',filenameltfb])
-  solution1remote=indice1+correction1;
-  SNR1remote=10*log10(SNR1r+SNR1i);           % two-way
+  filenameltfb=[localdirltfb,dirlistmatltfb(dirnummat).name]
+  dirlistendltfb=dir([localdirltfb,num2str(str2num(dirlistmatltfb(dirnummat).name(1:10))+180)(1:8),'*end']); % exclude remote*
+  dateltfb=str2num(dirlistendltfb(1).name(1:10));
+  if (isempty(dir([localdirltfb,dirlistmatltfb(dirnummat).name(1:10),'*txt'])))  % dont process files that have already been processed
+    % clear code
+    eval(['load ',filenameltfb])
+    if (isempty(strfind(filenameltfb,"C"))) % file resulting from Octave script processing
+      solution1lo=indice1+correction1;      % ranging
+      solution2lo=indice2+correction2;      % reference channel = loopback
+      SNR1lo=10*log10(SNR1r+SNR1i);
+      SNR2lo=10*log10(SNR2r+SNR2i);
+    else                                    % file resulting from C program processing
+      solution1lo=correction1';              % ranging
+      solution2lo=correction2';              % reference channel = loopback
+      SNR1lo=SNR1';
+      SNR2lo=SNR2';
+    end
+    df1lo=df1;
+    df2lo=df2;
+  % arbitrarily keep max(SNR)-10 to remove initial record when TX inactive
+    dateini=dateltfb-length(solution1lo);    % seconds since 1/1/1970 from loopback: storage end = final date + 1 s
 
-  foutname=strrep(dirlistmatltfb(dirnummat).name,'.mat.gz','ltfb.txt');
-  fout=fopen(foutname,"w"); 
-  fprintf(fout,"%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\tdelayrem\t\tdf1rem\tSNR1rem\r\n");
-  for p=1:length(SNR2i)
-     % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
-     fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateini+p*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1(p),SNR1remote(p));
-  end
-  ltfbtw=(solution1remote-solution2lo)/(2*Nint+1)/fs; % tw-loopback
-  fclose(fout);
-%%% the end for both LTFB
+  % filter to only keep relevant items
+    indices=find(SNR1lo>max(SNR1lo)-10);
+    res=(solution1lo(indices)-solution2lo(indices))/fs/(2*Nint+1);
+    [a,b]=polyfit([1:length(res)],res,2);
+    if (std(res-b.yf)>1e-6)
+       indices1=find((abs(res-mean(res(30:40))))<1e-5);   % 5 ns/s = 900 ns/3 min
+       indices2=find((abs(res-mean(res(140:150))))<1e-5);
+       if (!isempty(indices1) && (!isempty(indices2)))
+         if (length(indices1)>length(indices2)) indices=indices1;else indices=indices2;end;
+         res=res(indices);
+         [a,b]=polyfit([1:length(res)],res,2);
+         printf("keep %d samples ",length(indices));
+	 res(1:5)
+	 res(end-5:end)
+       end
+    end
+    printf("stdLTFB=%f\t",std((res-b.yf)*1e9));
+    moy=mean(res);
+    if (moy<0) moy=moy+1;end
+    printf("<LTFB>=%f\n",moy);
+  % end of filtering
 
-if (1==0)
-%%% start OP: remote
-  filenameop=dir([remotedirop,num2str(dateltfb)(1:end-2),'*']);
-  if (isempty(filenameop))
-     printf("No match !\n");
+  %%% start LTFB: remote
+    filenameltfb=[remotedirltfb,dirlistmatltfb(dirnummat).name];
+    eval(['load ',filenameltfb])
+    if (isempty(strfind(filenameltfb,"C")))
+      solution1remote=indice1+correction1;
+      SNR1remote=10*log10(SNR1r+SNR1i);           % two-way
+    else
+      solution1remote=correction1;
+      SNR1remote=SNR1;           % two-way
+    end
+  
+    foutname=strrep(dirlistmatltfb(dirnummat).name,'.mat.gz','ltfb.txt');
+    fout=fopen(foutname,"w"); 
+    fprintf(fout,"%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\tdelayrem\t\tdf1rem\tSNR1rem\r\n");
+    for p=indices % 1:length(SNR1lo)
+       % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
+       fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateini+(p-1)*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1(p),SNR1remote(p));
+    end
+    fclose(fout);
   else
-     printf("%s <> %s\n",filenameltfb,filenameop.name)
-     eval(['load ',remotedirop,filenameop.name])
-     solution1remote=indice1+correction1;
-     SNR1remote=10*log10(SNR1r+SNR1i);
-     df1remote=df1;
-     k=find(SNR1remote>max(SNR1remote)-10); 
-     dateiniop=dateiniltfb-k(1)+1; % ranging LTFB = ranging OP
-%%% start OP: local
-     clear code
-     eval(['load ',localdirop,filenameop.name])
-     solution1lo=indice1+correction1;
-     solution2lo=indice2+correction2;          % reference channel
-     df1lo=df1;
-     df2lo=df2;
-     SNR1lo=10*log10(SNR1r+SNR1i);
-     SNR2lo=10*log10(SNR2r+SNR2i);
-% arbitrarily keep max(SNR)-10 to remove initial record when TX inactive
-
-     foutname=strrep(filenameop.name,'.mat.gz','op.txt');
-     fout=fopen(foutname,"w"); 
-     fprintf(fout,"%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\tdelayrem\t\tdf1rem\tSNR1rem\r\n");
-     for p=1:length(SNR2i)
-     % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
-        fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateiniop+p*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1remote(p),SNR1remote(p));
-     end
-     fclose(fout);
-     optw=(solution1remote-solution2lo)(k)/(2*Nint+1)/fs; % tw-loopback
-     if (length(optw)<length(ltfbtw)) longueur=length(optw); else longueur=length(ltfbtw);end;
-     tw=optw(1:longueur)-ltfbtw(1:longueur);
-     moyenne(dirnummat)=mean(tw);
-     ecart(dirnummat)=std(tw);
-     ladate(dirnummat)=dateiniop;
-end
+    printf("already processed\n");
   end
+%%% the end for both LTFB
 end
-%%% the end for both OP
-return
-subplot(211);
-k=find(moyenne<-0.5);moyenne(k)=moyenne(k)+1;
-k=find(ladate>0);
-plot(ladate(k),moyenne(k)*1e9);ylim([60 100]);ylabel("<tw delay> (ns)")
-subplot(212);
-plot(ladate(k),ecart(k)*1e9);ylim([0 5]);ylabel("std(tw) (ns)");xlabel('unix time (s)')
--- processing/Octave/gofinal_op.m
+++ processing/Octave/gofinal_op.m
@@ -21,50 +21,20 @@ else
 end
 
 for dirnummat=1:length(dirlistmatop) 
-if (1==0)
-%%% start LTFB: local
-  filenameltfb=[localdirltfb,dirlistmatltfb(dirnummat).name];
-  dateltfb=str2num(dirlistendltfb(dirnummat).name(1:10));
-  clear code
-  eval(['load ',filenameltfb])
-  solution1lo=indice1+correction1;          % ranging
-  solution2lo=indice2+correction2;          % reference channel = loopback
-  df1lo=df1;
-  df2lo=df2;
-  SNR1lo=10*log10(SNR1r+SNR1i);
-  SNR2lo=10*log10(SNR2r+SNR2i);
-% arbitrarily keep max(SNR)-10 to remove initial record when TX inactive
-  dateini=dateltfb-length(solution1lo)+1; % seconds since 1/1/1970 from loopback
-%%% start LTFB: remote
-  filenameltfb=[remotedirltfb,dirlistmatltfb(dirnummat).name];
-  eval(['load ',filenameltfb])
-  solution1remote=indice1+correction1;
-  SNR1remote=10*log10(SNR1r+SNR1i);           % two-way
-
-  foutname=strrep(dirlistmatltfb(dirnummat).name,'.mat.gz','ltfb.txt');
-  fout=fopen(foutname,"w"); 
-  fprintf(fout,"%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\tdelayrem\t\tdf1rem\tSNR1rem\r\n");
-  for p=1:length(SNR2i)
-     % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
-     fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateini+p*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1(p),SNR1remote(p));
-  end
-  ltfbtw=(solution1remote-solution2lo)/(2*Nint+1)/fs; % tw-loopback
-  fclose(fout);
-%%% the end for both LTFB
-end
 
 %%% start OP: remote
-  filenameop=[remotedirop,dirlistmatop(dirnummat).name];
+  filenameop=[remotedirop,dirlistmatop(dirnummat).name]
   if (isempty(filenameop))
      printf("No match !\n");
   else
+   if (isempty(dir(['../',strrep(dirlistmatop(dirnummat).name,'.mat.gz','op.txt')])))
      eval(['load ',filenameop])
      solution1remote=indice1+correction1_a;
      SNR1remote=10*log10(SNR1r+SNR1i);
      df1remote=df1;
      k=find(SNR1remote>max(SNR1remote)-10); 
      dateop=str2num(dirlistmatop(dirnummat).name(12:21));
-     dateiniop=dateop-length(indice1)+1; % seconds since 1/1/1970 from loopback
+     dateiniop=dateop-length(indice1); % seconds since 1/1/1970 from loopback
 %%% start OP: local
      clear code
      eval(['load ',localdirop,dirlistmatop(dirnummat).name])
@@ -75,22 +45,40 @@ end
      SNR1lo=10*log10(SNR1r+SNR1i);
      SNR2lo=10*log10(SNR2r+SNR2i);
 % arbitrarily keep max(SNR)-10 to remove initial record when TX inactive
+     
+   % filter to only keep relevant items
+     indices=find(SNR1lo>max(SNR1lo)-10);
+     res=(solution1lo(indices)-solution2lo(indices))/fs/(2*Nint+1);
+     [a,b]=polyfit([1:length(res)],res,2);
+     if (std(res-b.yf)>1e-6)
+        indices1=find((abs(res-mean(res(30:40))))<1e-5);   % 5 ns/s = 900 ns/3 min
+        indices2=find((abs(res-mean(res(140:150))))<1e-5);
+        if (!isempty(indices1) && (!isempty(indices2)))
+          if (length(indices1)>length(indices2)) indices=indices1;else indices=indices2;end;
+          res=res(indices);
+          [a,b]=polyfit([1:length(res)],res,2);
+          printf("keep %d samples ",length(indices));
+	  res(1:5)
+	  res(end-5:end)
+	end
+     end
+     printf("stdOP=%f\t",std((res-b.yf)*1e9));
+     moy=mean(res);
+     if (moy<0) moy=moy+1;end
+     printf("<OP>=%f\n",moy);
+   % end of filtering
 
      foutname=strrep(dirlistmatop(dirnummat).name,'.mat.gz','op.txt');
      fout=fopen(foutname,"w"); 
      fprintf(fout,"%% y  m  d  h  m  s\tdelay\t\tdf1\tSNR1\tdf2\tSNR2\tdelayrem\t\tdf1rem\tSNR1rem\r\n");
      for p=1:length(SNR2i)
      % datestr(datenum([1970, 1, 1, 0, 0, 1670074201]))
-        fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateiniop+p*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1remote(p),SNR1remote(p));
+        fprintf(fout,"%s\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\t%.12f\t%.3f\t%.1f\n",strftime("%Y %m %d %H %M %S", localtime(dateiniop+(p-1)*(length(code)/2)/2.5e6)),solution1lo(p)/(2*Nint+1)/fs,df1lo(p),SNR1lo(p),solution2lo(p)/(2*Nint+1)/fs,df2lo(p),SNR2lo(p),solution1remote(p)/(2*Nint+1)/fs,df1remote(p),SNR1remote(p));
      end
      fclose(fout);
+   else
+     printf("Already done\n");
+   end
   end
 end
 %%% the end for both OP
-return
-subplot(211);
-k=find(moyenne<-0.5);moyenne(k)=moyenne(k)+1;
-k=find(ladate>0);
-plot(ladate(k),moyenne(k)*1e9);ylim([60 100]);ylabel("<tw delay> (ns)")
-subplot(212);
-plot(ladate(k),ecart(k)*1e9);ylim([0 5]);ylabel("std(tw) (ns)");xlabel('unix time (s)')
