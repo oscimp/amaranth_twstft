6964035068bdb7f51ac5113637cd434287eb795f
Jean-Michel Friedt <friedtj@free.fr>
2025-06-09 09:58:56 +0200

    update sampling script

--- acquisition/gosampling.sh
+++ acquisition/gosampling.sh
@@ -3,10 +3,13 @@ pid=`ps aux | grep multi_ | grep -v grep | cut -d\  -f2`; if [[ -n "$pid" ]]; th
 sleep 30
 echo "Start"
 ladate=`date +%s`
-nice -n -20 /home/jmfriedt/2401/rx_multi_samples > /data/$ladate.log 2>&1 &
-# /usr/bin/python3 /home/jmfriedt/2401/acq5min.py > /data/$ladate.log 2>&1 &
-echo "Waiting"
-wait
+touch /data/file2.bin
+while (test $( ls -s  /data/file2.bin | cut -d\  -f1 ) -le 10) do # repeat measurement until success
+  nice -n -20 /home/jmfriedt/2401/rx_multi_samples > /data/$ladate.log 2>&1 &
+  # /usr/bin/python3 /home/jmfriedt/2401/acq5min.py > /data/$ladate.log 2>&1 &
+  echo "Waiting"
+  wait
+done
 echo "Finished"
 ladatefin=`date +%s`
 fractionfin=`stat -c "%y" /data/file1.bin  | cut -d\.  -f2 | cut -d\  -f1`
@@ -17,3 +20,4 @@ fractionfin=`stat -c "%y" /data/file2.bin  | cut -d\.  -f2 | cut -d\  -f1`
 entierfin=`stat -c "%Y" /data/file2.bin`
 echo "${ladate}_${entierfin}.${fractionfin}"
 mv /data/file2.bin /data/${ladate}_${entierfin}.${fractionfin}_2.bin
+uhd_usrp_probe # reset RFNOC ?
